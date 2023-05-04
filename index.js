6981c9bf73745be9c58a68485a4d172bc4e119cd
Abiy Hussen Aman <AbiyHussen@gmail.com>
2023-05-04 13:23:13 +0300

    Linter and luxon is configured

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// import { DateTime } from './modules/luxon.js';
 import { DateTime } from './node_modules/luxon/src/luxon.js';
 import BookList from './modules/booklist.js';
 import { handleNavAddClick, handleNavListClick, handleNavContactClick } from './modules/navigation.js';
